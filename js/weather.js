418abd1064613685fcfdce527d6a3ca67c3f1ba0
yushinc <cys74351@gmail.com>
2023-01-19 16:53:05 +0900

    0119

--- js/weather.js
+++ js/weather.js
@@ -1,9 +1,28 @@
-function onGeoOk(position) {
+const API_KEY = "e3806d24e94386273bfe40417f67b80c";
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
 
+function onGeoOk(position) {
+    
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    
+    fetch(url) 
+        .then((response) => response.json())
+        .then((data) => {
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+        }); 
+    
+    // fetch(url) : call the url 
 }
+
+
 
 function onGeoError() {
     alert("Can't find you. No weather for you.");
 }
+
 
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError); // 현 좌표 제공
